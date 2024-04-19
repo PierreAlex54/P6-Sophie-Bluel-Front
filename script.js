e8c037fd153f35306b434a5d08281487ad1c5f88
Pierre Alex <pierrealexoc2023@gmail.com>
2024-04-19 16:33:49 +0200

    log page in progress

--- script.js
+++ script.js
@@ -17,6 +17,10 @@ async function createGallery() {
     const workFigure = document.createElement("figure");
     const workImg = document.createElement("img");
     const workCaption = document.createElement("figcaption");
+    // ajout de l'ID category à chaque figure
+    workFigure.dataset.workId = element.categoryId;
+    console.log(workFigure.dataset.workId);
+
     // ajout des elements à gallery
     gallery.appendChild(workFigure);
     workFigure.appendChild(workImg);
@@ -29,15 +33,61 @@ async function createGallery() {
 }
 createGallery();
 
-// // ajout contenu gallery
-// async function showGallery() {
-//   const works = await getWorks();
-//   const img = document.querySelectorAll(".gallery .img");
-//   const caption = document.querySelectorAll(".gallery .figcaption");
+// récupération catégories API
 
-//   for (let i = 0; i < works.length; i++) {
-//     img.src = works[i].imageUrl;
-//     caption.innerText = works[i].title;
-//   }
-// }
-// showGallery();
+async function getCategories() {
+  const response = await fetch("http://localhost:5678/api/categories");
+  const categories = await response.json();
+  console.log(categories);
+  return categories;
+}
+
+// création boutons catégories
+
+async function createCategories() {
+  const filters = document.createElement("div");
+  filters.className = "filters";
+  const gallery = document.querySelector(".gallery");
+  gallery.parentNode.insertBefore(filters, gallery);
+
+  const btnTous = document.createElement("button");
+  btnTous.innerText = "Tous";
+  btnTous.className = "filterBtn";
+  filters.appendChild(btnTous);
+
+  btnTous.dataset.btnId = 0;
+  console.log(btnTous.id);
+
+  const categories = await getCategories();
+  categories.forEach((element) => {
+    const filtersBtn = document.createElement("button");
+    filtersBtn.innerText = element.name;
+    filtersBtn.className = "filterBtn";
+    filters.appendChild(filtersBtn);
+
+    filtersBtn.dataset.btnId = element.id;
+    console.log(filtersBtn.dataset.btnId);
+  });
+  await activFilters();
+}
+createCategories();
+
+async function activFilters() {
+  const buttons = document.querySelectorAll(".filterBtn");
+  buttons.forEach((button) => {
+    button.addEventListener("click", async (event) => {
+      // récupération de l'ID du bouton cliqué
+      const btnId = event.target.dataset.btnId;
+      console.log(event.target.dataset.btnId);
+      const figures = document.querySelectorAll("#portfolio .gallery figure");
+
+      figures.forEach((figure) => {
+        if (btnId === "0" || figure.dataset.workId === btnId) {
+          figure.style.display = "block";
+        } else {
+          figure.style.display = "none";
+        }
+      });
+    });
+  });
+}
